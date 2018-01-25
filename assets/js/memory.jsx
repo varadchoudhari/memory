58d1a3bf2dd8fd16e57466da75e176ebb133caf6
Varad Choudhari <varad.choudhari@gmail.com>
2018-01-25 10:31:36 -0500

    added shuffler

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -8,36 +8,38 @@ export default function run_game(root) {
 
 class Square extends React.Component {
   showValue(id,value,handler, click_count) {
-    var button = document.getElementById(id);
+    let button = document.getElementById(id);
     button.style.background = "#FFF";
     button.innerHTML = value;
     if(click_count == 1) {
       handler(value, 0, id);
     }
     else {
-      var new_count = click_count + 1;
+      let new_count = click_count + 1;
       handler(value, new_count, id);
     }
 
   }
   hideValue(id,value,handler) {
-    var button = document.getElementById(id);
+    let button = document.getElementById(id);
     button.style.background = "#026DAE";
     button.innerHTML = "";
   }
-  analyse(id, value, matchtracker, handler, previous_click, click_count, previous_id, matched_ids, updatescore, getscore) {
-    var show = this.showValue(id,value,handler, click_count);
+  analyse(id, value, reset_previous, handler, previous_click, click_count, previous_id, updatescore, getscore) {
+    let show = this.showValue(id,value,handler, click_count);
 
-    var score = getscore + 1;
+    let score = getscore + 1;
     updatescore(score);
 
     document.getElementById(id).disabled = true;
 
     if(previous_click == value) {
-      var first = document.getElementById(id);
-      var second = document.getElementById(previous_id);
-      var newelement1 = document.createElement("div");
-      var newelement2 = document.createElement("div");
+      let first = document.getElementById(id);
+      let second = document.getElementById(previous_id);
+      let newelement1 = document.createElement("div");
+      let newelement2 = document.createElement("div");
+      newelement1.innerHTML = "✓";
+      newelement2.innerHTML = "✓";
       newelement1.setAttribute("id",id);
       newelement1.setAttribute("class","matchtile");
       newelement2.setAttribute("id",previous_id);
@@ -46,39 +48,34 @@ class Square extends React.Component {
         first.replaceWith(newelement1);
         second.replaceWith(newelement2);
       },500);
-      // first.disabled = true;
-      // var second = document.getElementById(previous_id);
-      // second.disabled = true;
-      matchtracker(id, previous_id);
     }
     else if(click_count == 1) {
-      var buttons = Array.from(document.getElementsByTagName("button"));
-      var matched = Array.from(matched_ids);
-      for(var i = 0; i < buttons.length; i++) {
+      let buttons = Array.from(document.getElementsByTagName("button"));
+      for(let i = 0; i < buttons.length; i++) {
           buttons[i].disabled = true;
       }
       setTimeout(() => {this.hideValue(id,value,handler);},1000);
       setTimeout(() => {this.hideValue(previous_id,value,handler);},1000);
       setTimeout(() => {
-        for(var i = 0; i < buttons.length; i++) {
+        for(let i = 0; i < buttons.length; i++) {
             buttons[i].disabled = false;
         }
       },1000);
+      reset_previous(); //function to reset previous entries
     }
   }
   render() {
-    var uid = this.props.uid;
-    var value = this.props.value;
-    var handler = this.props.handler;
-    var previous_click = this.props.previous;
-    var click_count = this.props.clickcount;
-    var previous_id = this.props.previousId;
-    var matchtracker = this.props.matchtracker;
-    var matched_ids = this.props.matched_ids;
-    var updatescore = this.props.updatescore;
-    var getscore = this.props.getscore;
+    let uid = this.props.uid;
+    let value = this.props.value;
+    let handler = this.props.handler;
+    let previous_click = this.props.previous;
+    let click_count = this.props.clickcount;
+    let previous_id = this.props.previousId;
+    let reset_previous = this.props.reset_previous;
+    let updatescore = this.props.updatescore;
+    let getscore = this.props.getscore;
 
-    return (<button id={uid} className="tile" onClick={() => this.analyse(uid, value, matchtracker, handler, previous_click, click_count, previous_id, matched_ids, updatescore, getscore)}>
+    return (<button id={uid} className="tile" onClick={() => this.analyse(uid, value, reset_previous, handler, previous_click, click_count, previous_id, updatescore, getscore)}>
     </button>);
   }
 }
@@ -87,28 +84,23 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.handler = this.handler.bind(this);
-    this.matchtracker = this.matchtracker.bind(this);
     this.updatescore = this.updatescore.bind(this);
+    this.reset_previous = this.reset_previous.bind(this);
     this.state = {
-      tile_values: ["A","B","C","D","A","E","F","B","D","C","F","E","G","H","H","G"],
+      tile_values: [],
       previously_clicked: "",
       previously_clicked_id: "",
       click_count: 0,
       score: 0,
-      matched_ids: []
     };
   }
 
   handler(button_value, click_count, id) {
     this.setState({previously_clicked: button_value, click_count: click_count, previously_clicked_id:id});
-    console.log(this.state.matched_ids);
   }
 
-  matchtracker(id1, id2) {
-    var clone = this.state.matched_ids.slice();
-    clone.push(id1);
-    clone.push(id2);
-    this.setState({matched_ids: clone});
+  reset_previous() {
+    this.setState({previously_clicked: "", previously_clicked_id: ""});
   }
 
   updatescore(score) {
@@ -116,15 +108,30 @@ class Board extends React.Component {
   }
 
   renderTile(id) {
-    var tile_value = this.state.tile_values[id];
-    var previous_value = this.state.previously_clicked;
-    var click_count = this.state.click_count;
-    var previous_id = this.state.previously_clicked_id;
-    var matched_ids = this.state.matched_ids;
-    var getscore = this.state.score;
-
-    return <Square uid={id} value={tile_value} matchtracker={this.matchtracker} handler = {this.handler} previous={previous_value} clickcount={click_count} previousId={previous_id} matched_ids={matched_ids} updatescore={this.updatescore} getscore={getscore}/>;
+    let tile_value = this.state.tile_values[id];
+    let previous_value = this.state.previously_clicked;
+    let click_count = this.state.click_count;
+    let previous_id = this.state.previously_clicked_id;
+    let getscore = this.state.score;
+    return <Square uid={id} value={tile_value} reset_previous={this.reset_previous} handler = {this.handler} previous={previous_value} clickcount={click_count} previousId={previous_id} updatescore={this.updatescore} getscore={getscore}/>;
   }
+
+  shuffleAndSet() {
+    let alphabets = ["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
+    let length = alphabets.length;
+    let temp = 0;
+    let index = 0;
+    while(length) {
+      index = Math.floor(Math.random() * length--);
+      temp = alphabets[length];
+      alphabets[length] = alphabets[index];
+      alphabets[index] = temp;
+    }
+    this.setState({tile_values: alphabets})
+    }
+    componentWillMount() {
+      this.shuffleAndSet();
+    }
   render() {
     return (
       <div id="board">
